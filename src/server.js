f18894fb883ca8e17899b0de7f947975acb2c5f8
M <rahmatjonovmahmudjonn@gmail.com>
2024-02-18 21:51:16 +0500

    changed some files for chacking new version

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import path from "path";
 
 const bot = new Telegraf(process.env.TOKEN);
 
-
 bot.command("help", (ctx) => {
   ctx.reply("how can i help you");
 });
@@ -17,27 +16,45 @@ bot.start((ctx) => {
 });
 
 bot.on(message("text"), async (ctx) => {
+  let platformUrl = "";
+  let apiHost = "";
+  let platform = "";
 
   const url = ctx.update.message.text;
-  const isValidUrl = url.startsWith("https://www.instagram.com/");
+  const isValidUrl = url.startsWith("https");
   if (!isValidUrl) {
     return ctx.reply("invalid url");
   }
-  
+  if (url.startsWith("https://www.instagram.com/")) {
+    platform = "instagram";
+    platformUrl = process.env.INSTAGRAM;
+    apiHost = process.env.INSTAGRAM_HOST;
+  } else if (url.startsWith("https://www.tiktok.com/")) {
+    platformUrl = process.env.TIKTOK;
+    apiHost = process.env.TIKTOK_HOST;
+  }
+
   const options = {
     method: "GET",
-    url: "https://instagram-media-downloader.p.rapidapi.com/rapid/post.php",
+    url: platformUrl,
     params: {
       url: url,
+      hd: '1'
     },
     headers: {
       "X-RapidAPI-Key": "5c845e0afbmshad04efb05f4cf5ep161576jsnfd86e565db9a",
-      "X-RapidAPI-Host": "instagram-media-downloader.p.rapidapi.com",
+      "X-RapidAPI-Host": apiHost,
     },
   };
   try {
     const response = await axios.request(options);
-    const videourl = response.data.video;
+    let videourl;
+    if (platform == "instagram") {
+      videourl = response.data.video;
+    } else {
+      videourl = response.data.play
+    }
+    console.log(videourl);
 
     const loadingvideo = await axios.get(videourl, { responseType: "stream" });
     const videoFileName = `${Date.now()}.mp4`;
@@ -48,6 +65,8 @@ bot.on(message("text"), async (ctx) => {
     videoFile.on("finish", () => {
       ctx.replyWithVideo({ source: path.join("public", videoFileName) });
       setTimeout(() => {
+        apiHost = "";
+        platformUrl = "";
         fs.unlink(path.join("public", videoFileName), (err) => {
           if (err) {
             throw new err();
